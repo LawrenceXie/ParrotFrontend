ae319a14d6d7290da37c8bed1253440166310090
Lawrence Xie <lawrence.xie90@gmail.com>
2019-09-05 20:56:12 -0400

    added Google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import { LandingPageContainer } from "./components/index";
+import ReactGA from "react-ga";
+
+function initializeReactGA() {
+  ReactGA.initialize("UA-147237550-1");
+  ReactGA.pageview("/homepage");
+}
 
 class App extends Component {
   render() {
+    initializeReactGA();
     return <LandingPageContainer />;
   }
 }
